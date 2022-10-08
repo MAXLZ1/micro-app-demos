7b3cd4aa2e67df889a053a2fb8baa1000805215f
MAXLZ <2251683416@qq.com>
2022-10-08 16:57:23 +0800

    build: update vue3-main production base

--- packages/qiankun-demo/vue3-main/vite.config.ts
+++ packages/qiankun-demo/vue3-main/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
-
+console.log(process)
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
