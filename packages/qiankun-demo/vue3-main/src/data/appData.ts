2e0cca84a12409c8ca5d844423d3d21b55eb28e5
MAXLZ <2251683416@qq.com>
2022-10-09 01:14:18 +0800

    fix: modify micro-app entry in production

--- packages/qiankun-demo/vue3-main/src/data/appData.ts
+++ packages/qiankun-demo/vue3-main/src/data/appData.ts
@@ -5,9 +5,9 @@ export interface MicoApp {
   activeRule: string
 }
 
-export const vue2AppEntry = import.meta.env.MODE !== 'production' ? 'https://maxlz1.github.io/micro-app-demos/qiankun-demo/vue2-child/dist/' : 'http://localhost:8091'
-export const reactAppEntry = import.meta.env.MODE !== 'production' ? 'https://maxlz1.github.io/micro-app-demos/qiankun-demo/react-child/dist/' : 'http://localhost:8092'
-export const viteAppEntry = import.meta.env.MODE !== 'production' ? 'https://maxlz1.github.io/micro-app-demos/qiankun-demo/vite-child/dist/' : 'http://localhost:8093'
+export const vue2AppEntry = import.meta.env.MODE === 'production' ? 'https://maxlz1.github.io/micro-app-demos/qiankun-demo/vue2-child/dist/' : 'http://localhost:8091'
+export const reactAppEntry = import.meta.env.MODE === 'production' ? 'https://maxlz1.github.io/micro-app-demos/qiankun-demo/react-child/dist/' : 'http://localhost:8092'
+export const viteAppEntry = import.meta.env.MODE === 'production' ? 'https://maxlz1.github.io/micro-app-demos/qiankun-demo/vite-child/dist/' : 'http://localhost:8093'
 
 export const apps = [
   {
