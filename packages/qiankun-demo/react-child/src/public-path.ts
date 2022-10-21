29d2370fd3474bd1e23e820be3622231a41d1d2a
MAXLZ <2251683416@qq.com>
2022-10-21 12:12:24 +0800

    feat: vue2 app add @type/node

--- packages/qiankun-demo/react-child/src/public-path.ts
+++ packages/qiankun-demo/react-child/src/public-path.ts
@@ -1,6 +1,6 @@
-/* eslint-disable */
+//* eslint-disable */
+// @ts-nocheck
 // @ts-ignore
 if (window.__POWERED_BY_QIANKUN__) {
-  // @ts-ignore
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
 }
