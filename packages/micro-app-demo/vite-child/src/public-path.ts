eb3950f21f53da994ef78d479df9eaf2e6d4fbb1
MAXLZ <2251683416@qq.com>
2022-10-21 14:51:10 +0800

    fix: fix micro-app-demo build error

--- packages/micro-app-demo/vite-child/src/public-path.ts
+++ packages/micro-app-demo/vite-child/src/public-path.ts
@@ -1,5 +1,6 @@
+/* eslint-disable */
+// @ts-nocheck
 // @ts-ignore
 if (window.__MICRO_APP_ENVIRONMENT__) {
-  // eslint-disable-next-line
   __webpack_public_path__ = window.__MICRO_APP_PUBLIC_PATH__
 }
