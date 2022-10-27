560849cfa23425edd6467e287eb82a5b960e7217
MAXLZ1 <MAXLZ1@users.noreply.github.com>
2022-10-27 09:37:49 +0000

    feat: garfish demo update navigate between apps

--- garfish-demo/vue3-main/assets/KeepAliveView.38302755.js
+++
@@ -1,1 +0,0 @@
-import{H as e,z as t,az as o}from"./index.3a29da17.js";const s={name:"KeepAliveView"},a={id:"keep-alive"};function c(n,i,r,p,_,d){return t(),o("div",a)}const f=e(s,[["render",c]]);export{f as default};
---
+++ garfish-demo/vue3-main/assets/KeepAliveView.eaeacfc7.js
@@ -0,0 +1,1 @@
+import{K as e,B as t,aA as o}from"./index.6d0babf4.js";const s={name:"KeepAliveView"},a={id:"keep-alive"};function c(n,i,r,p,_,d){return t(),o("div",a)}const f=e(s,[["render",c]]);export{f as default};
