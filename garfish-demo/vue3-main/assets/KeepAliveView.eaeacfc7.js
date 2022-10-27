4e2b6e1ff3f84c9a5e469a19b15185aa60af87eb
MAXLZ1 <MAXLZ1@users.noreply.github.com>
2022-10-27 11:44:17 +0000

    feat: garfish demo add keep alive app

--- garfish-demo/vue3-main/assets/KeepAliveView.eaeacfc7.js
+++
@@ -1,1 +0,0 @@
-import{K as e,B as t,aA as o}from"./index.6d0babf4.js";const s={name:"KeepAliveView"},a={id:"keep-alive"};function c(n,i,r,p,_,d){return t(),o("div",a)}const f=e(s,[["render",c]]);export{f as default};
